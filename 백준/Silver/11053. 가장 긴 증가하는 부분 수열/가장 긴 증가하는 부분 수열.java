2d378605666a59bf484073020225d9fdc8e1a7be
HyunGeun Kim <99gusrms@naver.com>
2024-12-05 20:00:30 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 124 ms, Memory: 14452 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+
+        int n = Integer.parseInt(br.readLine());
+        int[] a = new int[n];
+        int[] dp = new int[n];
+        int result = 0;
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+
+            for (int j = 0; j < i; j++) {
+                if (a[j] < a[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        for (int i : dp) {
+            result = Math.max(result, i);
+        }
+
+        System.out.println(result);
+    }
+}
