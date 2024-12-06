aa1bb4cc2980afa4d86f68aed0b07fa3ae1893f0
HyunGeun Kim <99gusrms@naver.com>
2024-12-06 14:05:11 +0900

    [Silver III] Title: N과 M (2), Time: 104 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static int n, m;
+    static boolean[] visited;
+    static ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        visited = new boolean[n + 1];
+
+        dfs(1, 0);
+
+        System.out.println(sb);
+    }
+
+    public static void dfs(int x, int depth) {
+        if (depth == m) {
+            for (Integer i : stack) {
+                sb.append(i).append(" ");
+            }
+
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = x; i < n + 1; i++) {
+            if (!visited[i]) {
+                stack.addLast(i);
+                visited[i] = true;
+                dfs(i + 1, depth + 1);
+                stack.removeLast();
+                visited[i] = false;
+            }
+        }
+    }
+}
