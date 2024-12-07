3f0730ba23d4bfceccf22155742c2833800287c0
HyunGeun Kim <99gusrms@naver.com>
2024-12-07 14:01:15 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 260 ms, Memory: 17028 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+
+        for (int i = 1; i < n + 1; i++) {
+            q.addLast(i);
+        }
+
+        while (!q.isEmpty()) {
+            for (int i = 0; i < m - 1; i++) {
+                q.addLast(q.removeFirst());
+            }
+
+            sb.append(q.removeFirst());
+
+            if (!q.isEmpty()) {
+                sb.append(", ");
+            }
+        }
+
+        System.out.println("<" + sb + ">");
+    }
+}
